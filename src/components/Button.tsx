e212c297bab0c7ffe34b06a9660056e8c8be525f
Pablo <pablo.valle@upflow.me>
2021-11-24 01:21:58 -0300

    Segunda aula

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonHTMLAttributes, useState } from 'react';
+
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
+	return <button className="button" {...props} />;
+}
